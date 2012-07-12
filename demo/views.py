5da93a3b2c0192eaa1f58dff0328af2d3b4367e8
Facundo Gaich <facugaich@gmail.com>
2012-07-11 23:10:22 -0300

    Rename widget test to field. Remove trace.

--- demo/views.py
+++ demo/views.py
@@ -2,19 +2,18 @@ from django.shortcuts import render
 
 from forms import WidgetTest
 
-def widget(request):
-    
+def field(request):
+
     template_dict = {}
 
     if request.method == 'POST':
-#        import pdb; pdb.set_trace()
         form = WidgetTest(request.POST)
         if form.is_valid():
             template_dict['result'] = form.cleaned_data['youtube_id']
             initial = {'youtube_id': template_dict['result']}
             template_dict['form'] = WidgetTest(initial=initial)
-        else: 
+        else:
             template_dict['form'] = form
     else:
         template_dict['form'] = WidgetTest(initial={'youtube_id': '123'})
-    return render(request, 'widget.html', template_dict)
+    return render(request, 'field.html', template_dict)
